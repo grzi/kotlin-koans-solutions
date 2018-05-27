c02ec3c6c70a4ef6a93bef89e73b42e14e763afe
grzi <jeremy.thulliez@gmail.com>
2018-05-27 13:06:54 +0200

    First part OK

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -8,11 +8,10 @@ fun example() {
 }
 
 val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
+    return this.orders.flatMap { e->e.products }.toSet()
 }
 
 val Shop.allOrderedProducts: Set<Product> get() {
     // Return all products that were ordered by at least one customer
-    todoCollectionTask()
+    return this.customers.flatMap { e-> e.orderedProducts }.toSet()
 }
