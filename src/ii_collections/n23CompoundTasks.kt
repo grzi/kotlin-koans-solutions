04ed58d790d0fefb4c9d12f2d9c624ccfb6eaff7
grzi <jeremy.thulliez@gmail.com>
2018-05-28 14:09:49 +0200

    Some more exercices done (65/93)

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -2,17 +2,23 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return this.customers.filter {
+       e -> e.orders.flatMap {
+            f->f.products
+        }.contains(product)
+    }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return this.orders.flatMap { e-> e.products.filter { e.isDelivered } }.maxBy { f->f.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+   return this.customers.fold(0, {
+       partResult, e-> e.orders.flatMap { f->f.products }.count { g->g ==product } + partResult
+   })
 }
--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,12 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate>{
+    override fun compareTo(other: MyDate): Int {
+        val thisDate = ("" + year + (if(month<10) "0" else "") + month + (if(dayOfMonth<10) "0" else "") + dayOfMonth);
+        val otherDate = ("" + other.year + (if(other.month<10) "0" else "") + other.month + (if(other.dayOfMonth<10) "0" else "") + other.dayOfMonth);
+        return thisDate.toInt() - otherDate.toInt()
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
@@ -10,4 +16,8 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate,override val endInclusive: MyDate): ClosedRange<MyDate>{
+    override fun contains(value: MyDate): Boolean {
+        return value >= start && value <= endInclusive
+    }
+}
